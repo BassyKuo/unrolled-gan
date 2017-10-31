c21ff748f6b27c6c08f26edf7d29ca1c32f0a43f
BassyKuo <aaammmyyy27@gmail.com>
2017-10-31 13:52:32 +0800

    unsupervised loss = sigmoid_binary_cross_entropy

--- train_gaussian_mixture/plot.py
+++ train_gaussian_mixture/plot.py
@@ -39,6 +39,46 @@ def plot_scatter(data, dir=None, filename="scatter", color="blue"):
 	pylab.ylim(-4, 4)
 	pylab.savefig("{}/{}.png".format(dir, filename))
 
+def plot_heatmap_fast(value, dir='plot', filename="heatmap", prob=True, suffix=''):
+    '''
+    [input format]
+      value[0,0] = value of (x0,y0)
+      value[0,1] = value of (x0,y1)
+      value[0,2] = value of (x0,y2)
+      ...
+      value[4,4] = value of (x4,y4)
+
+    [Usage]
+      mesh = np.mgrid[-4:4:0.1, -4:4:0.1].transpose(1,2,0).reshape(-1,2)
+      predict = sess.run( foo(x) , feed_dict={x:mesh} )
+      predict = predict.reshape(80,80).transpose(1,0)
+      predict = np.flip(predict, 0)
+      plot_heatmap_fast(predict, train_sample_directory, 'Degeneration_{}'.format(t), prob=False)
+    '''
+    check = np.zeros([])
+    assert len(value.shape) == 2
+    assert type(value) == type(check)
+    if os.path.exists(dir) is False:
+        os.mkdir(dir)
+    labels = list(np.arange(-4,4).astype(int))
+    labellist = [''] * 80
+    for i,l in enumerate(labels):
+        labellist[i*10] = l
+    labellist[-1] = 4
+
+    fig = pylab.gcf()
+    fig.set_size_inches(20.0, 16.0)
+    pylab.clf()
+    if prob == True:
+        if value.min() < 0 and value.max() > 1:
+            value = 1. / (1+np.exp(-value))
+        ax = sns.heatmap(value, cbar=True, xticklabels=labellist, yticklabels=labellist[::-1], cbar_kws={'ticks':[0,.2,.4,.6,.8,1]}, vmax=1, vmin=0)
+    else:
+        ax = sns.heatmap(value, cbar=True, xticklabels=labellist, yticklabels=labellist[::-1])
+    hmap = ax.get_figure()
+    pylab.title('{}\n{}'.format(filename, suffix), fontsize=24)
+    hmap.savefig(os.path.join(dir, "{}{}.png".format(filename, suffix)))
+
 def main():
 	num_samples = 10000
 	samples_true = sampler.gaussian_mixture_circle(num_samples, num_cluster=generator_params["config"]["num_mixture"], scale=2, std=0.2)
--- train_gaussian_mixture/train.py
+++ train_gaussian_mixture/train.py
@@ -8,14 +8,20 @@ from progress import Progress
 from model import discriminator_params, generator_params, gan
 from args import args
 from plot import plot_kde, plot_scatter
+from plot import plot_heatmap_fast   #for D vaildation
 
 def plot_samples(epoch, progress):
 	samples_g = gan.generate_x(10000, from_gaussian=True)
 	samples_g.unchain_backward()
 	samples_g = gan.to_numpy(samples_g)
+        mesh = np.mgrid[-4:4:0.1, -4:4:0.1].transpose(1,2,0).reshape(-1,2).astype(np.float32)
+        predict, _ = gan.discriminate(mesh / 2.0, apply_softmax=False)
+        predict = predict.reshape(80,80).transpose(1,0)
+        predict = cuda.to_cpu(np.flip(predict, 0).data)
 	try:
 		plot_scatter(samples_g, dir=args.plot_dir, filename="scatter_epoch_{}_time_{}min".format(epoch, progress.get_total_time()))
 		plot_kde(samples_g, dir=args.plot_dir, filename="kde_epoch_{}_time_{}min".format(epoch, progress.get_total_time()))
+                plot_heatmap_fast(predict, dir=args.plot_dir, filename="hmap_epoch_{}_time_{}min".format(epoch, progress.get_total_time()), prob=False)
 	except:
 		pass
 
@@ -39,6 +45,11 @@ def main():
 	if args.gpu_device != -1:
 		cuda.cupy.random.seed(args.seed)
 
+        # Plot groud truth gaussian_mixture distribution
+        samples_u = sampler.gaussian_mixture_circle(10*batchsize_u, generator_config.num_mixture, scale=scale, std=0.2)
+        plot_scatter(samples_u, dir=args.plot_dir, filename="scatter_groundtruth")
+        plot_kde(samples_u, dir=args.plot_dir, filename="kde_groundtruth")
+
 	# training
 	progress = Progress()
 	plot_samples(0, progress)
@@ -53,42 +64,81 @@ def main():
 			# unrolling
 			for k in xrange(discriminator_config.unrolling_steps):
 				# sample from data distribution
-				samples_u = sampler.gaussian_mixture_double_circle(batchsize_u, generator_config.num_mixture, scale=scale, std=0.2)
+				samples_u = sampler.gaussian_mixture_circle(batchsize_u, generator_config.num_mixture, scale=scale, std=0.2)
 				# sample from generator
 				samples_g = gan.generate_x(batchsize_g, from_gaussian=True)
 				samples_g.unchain_backward()
 
-				# unsupervised loss
+				# ### unsupervised loss = BCE( D(x)=1 ) + BCE( D(G(z))=0 ) ###
+                                # [desciption] https://i.imgur.com/cufjQGi.jpg
+                                #
+				# ===[ unsupervised loss FOR REAL DATA ]===
 				# D(x) = Z(x) / {Z(x) + 1}, where Z(x) = \sum_{k=1}^K exp(l_k(x))
 				# softplus(x) := log(1 + exp(x))
 				# logD(x) = logZ(x) - log(Z(x) + 1)
-				# 		  = logZ(x) - log(exp(log(Z(x))) + 1)
-				# 		  = logZ(x) - softplus(logZ(x))
+				#         = logZ(x) - log(exp(log(Z(x))) + 1)
+				#         = logZ(x) - softplus(logZ(x))
+				# ===[ unsupervised loss FOR FAKE DATA ]===
 				# 1 - D(x) = 1 / {Z(x) + 1}
 				# log{1 - D(x)} = log1 - log(Z(x) + 1)
-				# 				= -log(exp(log(Z(x))) + 1)
-				# 				= -softplus(logZ(x))
-				log_zx_u, activations_u = gan.discriminate(samples_u / scale, apply_softmax=False)
-				log_dx_u = log_zx_u - F.softplus(log_zx_u)
-				dx_u = F.sum(F.exp(log_dx_u)) / batchsize_u
-				loss_unsupervised = -F.sum(log_dx_u) / batchsize_u	# minimize negative logD(x)
-				log_zx_g, _ = gan.discriminate(samples_g / scale, apply_softmax=False)
-				loss_unsupervised += F.sum(F.softplus(log_zx_g)) / batchsize_u	# minimize negative log{1 - D(x)}
+				#               = -log(exp(log(Z(x))) + 1)
+				#               = -softplus(logZ(x))
+                                # --- [ unsupervised loss ] ---
+                                ### log_zx = d(x)
+                                ### D(x) = sigmoid(d(x)) = sigmoid(log_zx)
+                                log_zx_u, activations_u = gan.discriminate(samples_u / scale, apply_softmax=False)
+                                ## --[ activations_u = the output of (x->Linear->Relu) ]--
+                                # print ("activations_u: ", activations_u[0].shape)    #activations_u[0].shape = [100, 128]
+                                log_dx_u = log_zx_u - F.softplus(log_zx_u)  #The softplus function is the smooth approximation of ReLU.
+                                dx_u = F.sum(F.exp(log_dx_u)) / batchsize_u
+                                loss_unsupervised = -F.sum(log_dx_u) / batchsize_u	# minimize negative logD(x)
+                                log_zx_g, _ = gan.discriminate(samples_g / scale, apply_softmax=False)
+                                loss_unsupervised += F.sum(F.softplus(log_zx_g)) / batchsize_u	# minimize negative log{1 - D(x)}
+                                # update discriminator
+                                gan.backprop_discriminator(loss_unsupervised)
 
-				# update discriminator
-				gan.backprop_discriminator(loss_unsupervised)
+				# --- [ Sigmoid Cross Entropy ] ---
+                                # d_output_x, activations_u = gan.discriminate(samples_u / scale, apply_softmax=False)
+                                # d_output_g, _             = gan.discriminate(samples_g / scale, apply_softmax=False)
+                                # loss_discriminator = F.mean(-F.log( 1. / (1.+F.exp(-d_output_x)) ) + \
+                                #                      -F.log( 1. - ( 1. / (1.+F.exp(-d_output_g)) ) ) )
+                                # # update discriminator
+                                # gan.backprop_discriminator(loss_discriminator)
+
 
 				if k == 0:
 					gan.cache_discriminator_weights()
-					sum_loss_unsupervised += float(loss_unsupervised.data)
-					sum_dx_unlabeled += float(dx_u.data)
+					# --- [ unsupervised loss ] ---
+                                        # sum_loss_unsupervised += float(loss_unsupervised.data)
+                                        # sum_dx_unlabeled += float(dx_u.data)
+					# --- [ Sigmoid Cross Entropy ] ---
+                                        sum_loss_unsupervised += float(loss_discriminator.data)
+
+                                # plot G samples
+				# samples_g = gan.generate_x(10000, from_gaussian=True)
+				# samples_g.unchain_backward()
+				# samples_g = gan.to_numpy(samples_g)
+				# plot_scatter(samples_g, dir=args.plot_dir, filename="epoch_{}-{}_time_{}min_G({})_inD({})".format(epoch, t, progress.get_total_time(), t, k))
+				# # plot D update progress
+				# mesh = np.mgrid[-4:4:0.1, -4:4:0.1].transpose(1,2,0).reshape(-1,2).astype(np.float32)
+				# predict, _ = gan.discriminate(mesh / 2.0, apply_softmax=False)
+				# predict = predict.reshape(80,80).transpose(1,0)
+				# predict = cuda.to_cpu(np.flip(predict, 0).data)
+				# plot_heatmap_fast(predict, dir=args.plot_dir, filename="epoch_{}-{}_time_{}min_D({})".format(epoch, t, progress.get_total_time(), k), prob=False)
 
 			# generator loss
 			samples_g = gan.generate_x(batchsize_g, from_gaussian=True)
 			log_zx_g, activations_g = gan.discriminate(samples_g / scale, apply_softmax=False)
-			log_dx_g = log_zx_g - F.softplus(log_zx_g)
-			dx_g = F.sum(F.exp(log_dx_g)) / batchsize_g
-			loss_generator = -F.sum(log_dx_g) / batchsize_u	# minimize negative logD(x)
+                        ## --[ activations_g = the output of (G(z)->Linear->Relu) ]--
+                        # print ("activations_g: ", activations_g[0].shape)    #activations_g[0].shape = [100, 128]
+                        # --- [ unsupervised loss ] ---
+                        # log_dx_g = log_zx_g - F.softplus(log_zx_g)  #The softplus function is the smooth approximation of ReLU.
+                        # dx_g = F.sum(F.exp(log_dx_g)) / batchsize_g
+                        # loss_generator = -F.sum(log_dx_g) / batchsize_u	# minimize negative logD(x)
+                        # --- [ Sigmoid Cross Entropy ] ---
+                        loss_generator = F.mean(-F.log( 1. / (1.+F.exp(-log_zx_g)) ))
+
+
 
 			# feature matching
 			if discriminator_config.use_feature_matching:
@@ -107,7 +157,7 @@ def main():
 			gan.restore_discriminator_weights()
 
 			sum_loss_adversarial += float(loss_generator.data)
-			sum_dx_generated += float(dx_g.data)
+			# sum_dx_generated += float(dx_g.data)
 			if t % 10 == 0:
 				progress.show(t, num_updates_per_epoch, {})
 
